f6c89bd4a73777c9631f3d4a4447f8819e3e1813
Saad Qureshi <saad.qu01@gmail.com>
2024-06-06 16:52:43 +0500

    feat: removed hardcoded path

--- src/features/api/authorsApiSlice.tsx
+++ src/features/api/authorsApiSlice.tsx
@@ -2,37 +2,38 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import constants from '../../constants';
 import { Author, AuthorResponse, UpdateAuthorRequest } from '../../interface';
 
-// Define our single API slice object
+const URL_PATH = '/authors';
+
 export const authorsApiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: `/${constants.NAMESPACE }`}),
+  baseQuery: fetchBaseQuery({ baseUrl: `/${constants.NAMESPACE}` }),
   endpoints: (builder) => ({
     getAuthors: builder.query<AuthorResponse, void>({
-      query: () => '/authors',
+      query: () => URL_PATH,
     }),
     getAuthor: builder.query<{ authors: Author }, number | string>({
-      query: (id) => `/authors/${id}`,
+      query: (id) => `${URL_PATH}/${id}`,
     }),
     addAuthor: builder.mutation<Author, Partial<Author>>({
       query: (author: Author) => ({
-        url: '/authors',
+        url: URL_PATH,
         method: 'POST',
         body: author,
       }),
     }),
     updateAuthor: builder.mutation<Author, UpdateAuthorRequest>({
       query: ({ author, id }) => ({
-        url: `/authors/${id}`,
+        url: `${URL_PATH}/${id}`,
         method: 'PATCH',
         body: author,
       }),
     }),
     deleteAuthor: builder.mutation<Author, number | string>({
-        query: (id) => ({
-          url: `/authors/${id}`,
-          method: 'DELETE',
-        }),
+      query: (id) => ({
+        url: `${URL_PATH}/${id}`,
+        method: 'DELETE',
       }),
+    }),
   }),
 });
 
